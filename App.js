69a7cb13800893d38e4b03710a72675f5b92ca8d
[Eugene] <[margera.fun@mail.ru]>
2018-06-16 23:20:04 +0300

    Add text input to main component

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default class App extends React.Component {
+  state = {
+    placeName: ''
+  };
+
+  placeNameChangedHandler = val => {
+    this.setState({
+      placeName: val
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+           style={{width: 300, borderColor: 'black', borderWidth: 1}}
+           value={this.state.placeName}
+            onChangeText={this.placeNameChangedHandler} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+});
