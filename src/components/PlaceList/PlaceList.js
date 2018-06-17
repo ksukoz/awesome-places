9d2f6a68ea2c6f8449e564e00f4a115e470ecb43
[Eugene] <[margera.fun@mail.ru]>
2018-06-17 21:10:26 +0300

    Change scroll view to flat list

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
-import { StyleSheet, ScrollView } from 'react-native';
+import { StyleSheet, ScrollView, FlatList } from 'react-native';
 
 import ListItem from '../ListItem/ListItem';
 
 const PlaceList = props => {
-  const placesOutput = props.places.map((place, i) => (
-    <ListItem key={i} placeName={place} onItemPressed={() => props.onItemDeleted(i)}/>
-  ));
 
-  return <ScrollView style={styles.listContainer}>{placesOutput}</ScrollView>
+  return <FlatList 
+            style={styles.listContainer}
+            data={props.places}
+            renderItem={(info) => (
+              <ListItem 
+                  placeName={info.item.value} 
+                  onItemPressed={() => props.onItemDeleted(info.item.key)}/>
+            )} />
 };
 
 const styles = StyleSheet.create({
