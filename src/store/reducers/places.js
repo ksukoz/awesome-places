e3cb260df9fc50d6547f53fd36dbd7c300ade55e
[Eugene] <[margera.fun@mail.ru]>
2018-06-18 12:55:45 +0300

    Set up reducer

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,3 +1,5 @@
+import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from '../actions/actionTypes';
+
 const initialState = {
   places: [],
   selectedPlace: null
@@ -5,6 +7,41 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_PLACE:
+      return {
+        ...state,
+        places: state.places.concat({
+          key: Math.random(),
+          name: action.placeName,
+          image: {
+            uri: 'https://images.pexels.com/photos/220769/pexels-photo-220769.jpeg?auto=compress&cs=tinysrgb&h=650&w=940'
+          }
+        })
+      };
+    
+    case DELETE_PLACE:
+      return {
+        ...state,
+        places: state.places.filter((place) => {
+          return place.key !== state.selectedPlace.key;
+        }),
+        selectedPlace: null
+      };
+
+    case SELECT_PLACE: 
+      return {
+        ...state,
+        selectedPlace: state.places.find(place => {
+          return place.key === action.placeKey
+        })
+      };
+    
+    case DESELECT_PLACE:
+      return {
+        ...state,
+        selectedPlace: null
+      };
+
     default: 
       return state;
   }
